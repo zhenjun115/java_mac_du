6c51e28d5a9f8ee294f720bb8aa4d2fdf48c2c1b
zhenjun115 <424087283@qq.com>
2017-01-20 10:54:59 +0800

    file storages info;

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,10 +1,24 @@
 package com;
 
+import java.nio.file.FileStore;
+import java.nio.file.FileSystems;
+
 public class Main
 {
     public static void main( String[] args )
     {
         String userHome = System.getProperty( "user.name" );
         System.out.printf( "Hello Du: %n", userHome );
+        try {
+            for (FileStore store: FileSystems.getDefault().getFileStores()) {
+                long total = store.getTotalSpace() / 1024;
+                long used = (store.getTotalSpace() - store.getUnallocatedSpace()) / 1024;
+                long avail = store.getUsableSpace() / 1024;
+                System.out.format("%-20s %12d %12d %12d%n", store, total, used, avail);
+            }
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
+        
     }
 }
